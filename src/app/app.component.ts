d89782a0df56d984e62f0bb3801cb2727d83896c
Juan diego <diiegooliveros@gmail.com>
2019-02-13 22:01:23 -0500

    Pipe slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  nombre = 'Diego';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
